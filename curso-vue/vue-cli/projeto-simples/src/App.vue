f75554a0ed901f1c2493cc708be005c0d14627dc
almerindopaixao <almerindo_paixao@live.com>
2021-01-30 11:13:36 -0300

    update vue-cli

---
+++ curso-vue/vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">Alterar2</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        titulo: 'Teste Data usando Vue Js'
+      };
+    }
+  }
+</script>
+
+<style>
+  #app {
+    background-color: chocolate;
+    color: #fff;
+  }
+</style>
