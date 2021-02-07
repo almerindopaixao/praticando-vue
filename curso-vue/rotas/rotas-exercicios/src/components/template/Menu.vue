31bf1ca61a4ea4b83cfdb8a1ab7182962c338968
almerindopaixao <almerindo_paixao@live.com>
2021-02-07 19:59:05 -0300

    Adicionando vue router

---
+++ curso-vue/rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="menu">
+      <ul>
+            <router-link to="/" tag="li" exact active-class="active">
+                <a>Inicio</a>
+            </router-link>
+            <router-link to="/usuario" tag="li" active-class="active">
+                <a>Usuário</a>
+            </router-link>
+        </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ curso-vue/rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="usuario-detalhe">
+      <h3>Usuário Detalhe</h3>
+      <p><strong>Código: </strong> {{ id }}</p>
+      <!-- :to="`/usuario/${id}/editar`"  -->
+      <router-link :to="{ name: 'editarUsuario', params: { id }, 
+      query: { completo: true, lingua: 'pt' }, hash: '#rodape' }" tag="button" primario>
+          Editar
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+
+    beforeRouteEnter(to, from, next) {
+        console.log('Dentro do componente usuario Detalhe');
+        next(vm => {
+            console.log(vm.id);
+        });
+    }
+
+    // data() {
+    //     return {
+    //         id: this.$route.params.id,
+    //     };
+    // },
+
+    // watch: {
+    //     $route(to) {
+    //         this.id = to.params.id;
+    //     },
+    // },
+}
+</script>
+
+<style>
+
+</style>
