bd44f1e29bfe64ef13130995794ed52bb2b23304
almerindopaixao <almerindo_paixao@live.com>
2021-02-08 00:17:07 -0300

    Adicionando vuex

---
+++ curso-vue/vuex/vuex-exercicios/src/components/Resumo.vue
@@ -0,0 +1,39 @@
+<template>
+    <Painel titulo="Resumo" roxo>
+        <div class="resumo">
+            <span>Total: <strong>{{ total | dinheiro }}</strong></span>
+            <hr>
+            <button>Finalizar!</button>
+        </div>
+    </Painel>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    // computed: {
+    //     total() {
+    //         return this.$store.getters.valorTotal;
+    //     },
+    // },
+    // computed: mapGetters(['valorTotal']),
+
+    computed: {
+        ...mapGetters({
+            total: 'valorTotal',
+        }),
+    }
+}
+</script>
+
+<style>
+    table {
+        width: 100%;
+    }
+
+    td {
+        border-top: 1px solid #EEE;
+        width: 33%;
+    }
+</style>
---
+++ curso-vue/vuex/vuex-exercicios/src/store/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import carrinho from './modules/carrinho';
+import parametros from './modules/parametros';
+
+import * as getters from './getters';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+      nome: 'ALMERINDO',
+      sobrenome: 'Paixão',
+  },
+
+  getters,
+
+  modules: { carrinho, parametros },
+});
